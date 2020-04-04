4a2536fcabb1dcbb417ac7d88a9654c39bac1b12
Romain Champourlier <public@rchampourlier.com>
2020-04-05 00:12:42 +0200

    Logging all messages in all mailboxes

--- main.go
+++ main.go
@@ -3,21 +3,19 @@ package main
 import (
 	"log"
 	"os"
+	"strings"
 
 	"github.com/emersion/go-imap"
 	"github.com/emersion/go-imap/client"
 )
 
 func main() {
-	log.Println("Connecting to server...")
-
 	// Connect to server
 	server := os.Getenv("SERVER")
 	c, err := client.DialTLS(server, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println("Connected")
 
 	// Don't forget to logout
 	defer c.Logout()
@@ -25,61 +23,55 @@ func main() {
 	// Login
 	email := os.Getenv("EMAIL")
 	password := os.Getenv("PASSWORD")
-	log.Println("connect to:", server)
-	log.Println("with email:", email)
 
 	if err := c.Login(email, password); err != nil {
 		log.Fatal(err)
 	}
-	log.Println("Logged in")
 
 	// List mailboxes
 	mailboxes := make(chan *imap.MailboxInfo, 10)
 	done := make(chan error, 1)
 	go func() {
 		done <- c.List("", "*", mailboxes)
-
 	}()
 
-	log.Println("Mailboxes:")
+	mailboxNames := make([]string, 0)
 	for m := range mailboxes {
-		log.Println("* " + m.Name)
+		mailboxNames = append(mailboxNames, m.Name)
 	}
 
 	if err := <-done; err != nil {
 		log.Fatal(err)
 	}
 
-	// Select INBOX
-	mbox, err := c.Select("INBOX", false)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Flags for INBOX:", mbox.Flags)
+	for _, m := range mailboxNames {
+		mbox, err := c.Select(m, false)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("* " + m)
 
-	// Get the last 4 messages
-	from := uint32(1)
-	to := mbox.Messages
-	if mbox.Messages > 3 {
-		// We're using unsigned integers here, only substract if the result is > 0
-		from = mbox.Messages - 3
-	}
-	seqset := new(imap.SeqSet)
-	seqset.AddRange(from, to)
+		if mbox.Messages > 0 {
+			// Get all messages
+			from := uint32(1)
+			to := mbox.Messages
+			seqset := new(imap.SeqSet)
+			seqset.AddRange(from, to)
 
-	messages := make(chan *imap.Message, 10)
-	done = make(chan error, 1)
-	go func() {
-		done <- c.Fetch(seqset, []imap.FetchItem{imap.FetchEnvelope}, messages)
-	}()
+			messages := make(chan *imap.Message, 10)
+			done = make(chan error, 1)
+			go func() {
+				done <- c.Fetch(seqset, []imap.FetchItem{imap.FetchEnvelope}, messages)
+			}()
 
-	log.Println("Last 4 messages:")
-	for msg := range messages {
-		log.Println("* " + msg.Envelope.Subject)
-	}
+			for msg := range messages {
+				log.Println("  - " + msg.Envelope.Subject + " (" + strings.Join(msg.Flags, ",") + ")")
+			}
 
-	if err := <-done; err != nil {
-		log.Fatal(err)
+			if err := <-done; err != nil {
+				log.Fatal(err)
+			}
+		}
 	}
 
 	log.Println("Done!")
